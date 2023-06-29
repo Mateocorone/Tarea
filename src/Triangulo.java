98cda2e5220b473ec65906f4851a38c38866b2c7
Mateocorone <105876437+Mateocorone@users.noreply.github.com>
2023-06-29 11:10:11 -0500

    Update Triangulo.java

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Triangulo {
+class Triangulo extends Figura {
+    private double lado1;
+    private double lado2;
+    private double lado3;
+
+    public Triangulo(double lado1, double lado2, double lado3) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+
+    @Override
+    double calcularPerimetro() {
+        return lado1 + lado2 + lado3;
+    }
+
+    @Override
+    double calcularArea() {
+        double semiperimetro = calcularPerimetro() / 2;
+        return Math.sqrt(semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - lado3));
+    }
 }
