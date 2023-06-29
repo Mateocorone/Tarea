b9e431b289b947bfed723d0717a9d737a925922b
Mateocorone <105876437+Mateocorone@users.noreply.github.com>
2023-06-29 11:07:33 -0500

    Update Circulo.java

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Circulo {
+class Circulo extends Figura {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    @Override
+    double calcularArea() {
+        return Math.PI * radio * radio;
+    }
 }
