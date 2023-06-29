255444dda3a0b18cc94443811918734e20fe8f5f
Mateocorone <105876437+Mateocorone@users.noreply.github.com>
2023-06-29 11:11:18 -0500

    Update Punto.java

--- src/Punto.java
+++ src/Punto.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Punto {
+import java.util.Scanner;
+
+class Punto {
+    private double x;
+    private double y;
+    private double z;
+
+    public Punto(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
 }
